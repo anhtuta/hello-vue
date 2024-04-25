e19b43a947f832e3bfdaa73bd990f857201381ec
anhtuta <anhtu51195@gmail.com>
2024-04-25 15:19:16 +0700

    component: add new post

--- src/components/essentials/ComponentsBasics.vue
+++ src/components/essentials/ComponentsBasics.vue
@@ -8,10 +8,33 @@ const posts = ref([
     { id: 3, title: 'Why Vue is so fun', content: 'Another one' }
 ])
 const postFontSize = ref(1)
+
+const newPostTitle = ref('')
+const newPostContent = ref('')
+const createNewPost = () => {
+    posts.value.push({
+        id: posts.value.length + 1,
+        title: newPostTitle.value,
+        content: newPostContent.value,
+        author: 'Anonymous'
+    })
+
+    // Clear input fields
+    newPostTitle.value = ''
+    newPostContent.value = ''
+}
 </script>
 
 <template>
     <h1>Components Basics</h1>
+
+    <h3>Add new post</h3>
+    <div class="add-new-post">
+        <input type="text" v-model="newPostTitle" placeholder="Enter title" />
+        <textarea v-model="newPostContent" placeholder="Enter content..."></textarea>
+        <button @click="createNewPost">Add post</button>
+    </div>
+
     <div :style="{ fontSize: postFontSize + 'em' }">
         <BlogPost v-for="post in posts" :key="post.id" :title="post.title" :author="post.author"
             @enlarge-text="postFontSize += 0.1" @shrink-text="postFontSize -= 0.1">
@@ -20,3 +43,17 @@ const postFontSize = ref(1)
         </BlogPost>
     </div>
 </template>
+
+
+<style scoped>
+.add-new-post {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    margin-bottom: 20px;
+}
+
+.add-new-post textarea {
+    height: 50px;
+}
+</style>
